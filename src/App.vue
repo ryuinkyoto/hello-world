110574cf99d94890c87bc57fab4baf42e52a12c7
류상욱 <ryuinkyoto@gmail.com>
2020-09-15 16:25:57 +0900

    test

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <input v-model="message" type="text" />
+    <h2 class="message">{{ message }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello world!",
+    };
+  },
+};
+</script>
+
+<style>
+.message {
+  font-size: 50px;
+  font-family: "Roboto", sans-serif;
+  color: blue;
+}
+</style>
